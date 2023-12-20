045022c7f24639e92616c5d2485639f97b18612d
Flávio Marques <flaviomarques2@hotmail.com>
2023-12-19 21:07:51 -0300

    arquivos iniciais do projeto e-commerce

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <>
+            <h1>Dashboard!</h1>
+            <Button>Click Here!</Button>
+        </>
+    )
+}
